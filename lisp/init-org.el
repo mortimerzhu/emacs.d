9c1b6cbdcc86e0643d0f689f34e2fb4ef5ef50d0
Chenyang Zhu <zhuchenyang118@163.com>
2021-01-05 15:20:04 +0800

    refine org-M-RET to not split line

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -203,4 +203,5 @@ skip user's own code in `org-mode-hook'."
 (global-set-key (kbd "C-c C-r") 'org-store-link)
 (setq vc-follow-symlinks nil)
 (setq org-agenda-files  '("~/GTD"))
+(setq org-M-RET-may-splite-line nil)
 (provide 'init-org)
